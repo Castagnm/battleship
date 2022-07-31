1f6725c4e3a0704161a1cc2d2b083b95bf241ec8
Maxime Castagnet <maxime.castagnet@wanadoo.fr>
2022-07-31 19:56:43 +0200

    App update

--- src/main/java/com/mcas/App.java
+++ src/main/java/com/mcas/App.java
@@ -1,13 +1,27 @@
 package com.mcas;
 
+import com.mcas.game.controller.ShipController;
+import com.mcas.game.model.ship.Ship;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    private static ShipController shipController;
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        shipController = ShipController.getInstanceOf();
+
+        Ship[] fleet = shipController.getFleet();
+
+        for(Ship next : fleet) {
+            System.out.println(next.toString());
+        }
     }
 }
--- src/main/java/com/mcas/game/model/ship/Target.java
+++ src/main/java/com/mcas/game/model/ship/Target.java
@@ -32,6 +32,6 @@ public class Target {
     
     @Override
     public String toString() {
-        return "Target " + coordX.toString() + coordY.toString();
+        return coordX.toString() + coordY.toString();
     }
 }
